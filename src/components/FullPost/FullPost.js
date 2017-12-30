6200dea45931d2ad3cc9cbb71398fd075597ed71
Radomir Slowik <radesl@interia.eu>
2017-12-30 22:43:27 +0100

    linded project

--- src/components/FullPost/FullPost.js
+++ src/components/FullPost/FullPost.js
@@ -1,29 +1,23 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
-import './style.scss'
+import React from "react";
+import { Link } from "react-router-dom";
+import "./style.scss";
 
 class FullPost extends React.Component {
-    render() {
-        const {
-            userId,
-            title,
-            body,
-            isRequesting
-        }=this.props
-        return (
-            isRequesting
-            ? <span>...Loading</span>
-            : <div className='FullPost'>
-                <div className='FullPost_userId'>{userId}</div>
-                <div className='FullPost_title'>{title}</div>
-                <div className='FullPost_body'>{body}</div>
-                <button className='FullPost_Link'>
-                    <Link to='/'>back to list</Link>
-                </button>
-            </div>
-
-        )
-    }
+  render() {
+    const { userId, title, body, isRequesting } = this.props;
+    return isRequesting ? (
+      <span>...Loading</span>
+    ) : (
+      <div className="FullPost">
+        <div className="FullPost_userId">{userId}</div>
+        <div className="FullPost_title">{title}</div>
+        <div className="FullPost_body">{body}</div>
+        <button className="FullPost_Link">
+          <Link to="/">back to list</Link>
+        </button>
+      </div>
+    );
+  }
 }
 
-export default FullPost
+export default FullPost;
--- src/components/PostList/PostListContainer.js
+++ src/components/PostList/PostListContainer.js
@@ -1,68 +1,66 @@
-import React from 'react'
-import { Switch, Route } from 'react-router-dom'
-import { connect } from 'react-redux'
-import * as DATA_REDUCER from './../../reducers/reducers'
-import PostList from './PostList'
-import './style.scss'
+import React from "react";
+import { Switch, Route } from "react-router-dom";
+import { connect } from "react-redux";
+import * as DATA_REDUCER from "./../../reducers/reducers";
+import PostList from "./PostList";
+import "./style.scss";
 
 class PostListContainer extends React.Component {
-    constructor() {
-        super()
-        this.showPostListOnSite = this.showPostListOnSite.bind(this)
-    }
+  constructor() {
+    super();
+    this.showPostListOnSite = this.showPostListOnSite.bind(this);
+  }
 
-    componentDidMount(){
-        const {fetchData}=this.props
-        fetchData('https://jsonplaceholder.typicode.com/posts')
-    }
+  componentDidMount() {
+    const { fetchData } = this.props;
+    fetchData("https://jsonplaceholder.typicode.com/posts");
+  }
 
-    showPostListOnSite(){
-        const {response}=this.props
-        return response && response.map((post)=>{
-            return <PostList
-                post={post}
-                key={post.id}
-                />
+  showPostListOnSite() {
+    const { response } = this.props;
+    return (
+      response &&
+      response.map(post => {
+        return <PostList post={post} key={post.id} />;
+      })
+    );
+  }
+  render() {
+    const showPostListOnSite = this.showPostListOnSite();
+    const { response, isRequesting } = this.props;
+    return isRequesting ? (
+      <span>...Loading</span>
+    ) : (
+      <ul className="PostListContainer">{showPostListOnSite}</ul>
+    );
+  }
+}
+
+const mapStateToProps = state => {
+  const { response, isRequesting } = state;
+  return { response, isRequesting };
+};
+
+const mapDispatchToProps = dispatch => {
+  return {
+    fetchData: url => {
+      dispatch({ type: DATA_REDUCER.FETCH_DATA_START });
+      fetch(url)
+        .then(response => response.json())
+        .then(response => {
+          dispatch({
+            type: DATA_REDUCER.FETCH_DATA_SUCESS,
+            response
+          });
         })
+        .catch(error => {
+          dispatch({
+            type: DATA_REDUCER.FETCH_DATA_FALIURE,
+            error
+          });
+        });
     }
-    render() {
-        const showPostListOnSite = this.showPostListOnSite()
-        const { response, isRequesting } = this.props
-        return (
-            isRequesting
-            ? <span>...Loading</span>
-            : <ul className='PostListContainer'>
-                {showPostListOnSite}
-            </ul>
-        )
-    }
-}
+  };
+};
 
-const mapStateToProps = (state) => {
-    const { response, isRequesting } = state
-    return { response, isRequesting }
-}
-
-const mapDispatchToProps = (dispatch) => {
-    return {
-        fetchData: (url) => {
-            dispatch({ type: DATA_REDUCER.FETCH_DATA_START })
-            fetch(url)
-                .then(response => response.json())
-                .then(response => {
-                    dispatch({
-                        type: DATA_REDUCER.FETCH_DATA_SUCESS,
-                        response
-                    })
-                })
-                .catch(error => {
-                    dispatch({
-                        type: DATA_REDUCER.FETCH_DATA_FALIURE,
-                        error
-                    })
-                })
-        }
-    }
-}
-
-export default connect(mapStateToProps, mapDispatchToProps)(PostListContainer)
+export default connect(mapStateToProps, mapDispatchToProps)(PostListContainer);
